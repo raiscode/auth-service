0c99f60b41657a57fe743fa0c598558a48a4c93b
“raiscodes” <raiscodes@gmail.com>
2024-01-13 17:01:12 +0530

    add express js

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+// Express app Instance
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Auth Service");
+});
+
+export default app;

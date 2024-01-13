430015cfaa64ee4b5aa7190b118c90a57ea3de22
“raiscodes” <raiscodes@gmail.com>
2024-01-13 16:15:59 +0530

    test husky 3

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function login(username: string): string {
-
+    // console.log(username);
+    const user = {
+        name:"Rais",
+    };
+    const name = user['name']
     return username
     
 }
